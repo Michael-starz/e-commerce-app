48910960f7a19f0ba7dbf076e3905ab6192970ed
Michael-starz <sirmichaelchibuike@gmail.com>
2026-04-07 17:03:32 +0100

    middleware debuging

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -7,15 +7,19 @@ export const protect = async (req, res, next) => {
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
-  ) {
+  ) 
+  console.log("Header received:", req.headers.authorization);
+  {
     try {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log("Decoded Token ID:", decoded.id);
 
       req.user = await User.findById(decoded.userId).select("-password"); // attach user to request
       next();
     } catch (error) {
+      console.log("JWT Verify Failed:", error.message);
       return res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
